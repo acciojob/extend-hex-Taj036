d8d20cc98b38812547e4a0017eb2ca6997c4b547
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-06 23:41:13 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+    // Remove the "#" if present and convert to lowercase
+    const normalizedHex = shortHex.replace(/^#/, '').toLowerCase();
+
+    // Validate the length of the short hex code
+    if (normalizedHex.length !== 3) {
+        throw new Error('Invalid short hex code. It should have exactly 3 characters.');
+    }
+
+    // Repeat each character to generate the full hex code
+    const fullHex = normalizedHex
+        .split('')
+        .map(char => char.repeat(2))
+
+		
+        .join('');
+
+    return `#${fullHex}`;
+}
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
